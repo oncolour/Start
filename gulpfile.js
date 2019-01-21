b0fc71ccd9c51592fd9a66f8cef5e70ac22466fd
oscarn <oscar.nilsson@sis.se>
2019-01-21 13:32:33 +0100

    source maps and defualt tasks plus clean build

--- gulpfile.js
+++ gulpfile.js
@@ -8,48 +8,71 @@ const sourcemaps = require('gulp-sourcemaps');
 const rename = require("gulp-rename");
 const prefix = require("gulp-autoprefixer");
 const uglify = require("gulp-uglify");
-//const { watch, series } = require('gulp');
+const del = require("del");
  
 gulpSass.compiler = require('node-sass');
 
-// Sources for app specific .js and .scss files.
-const jsAppSrc = './src/js/*.js';
-const scssAppSrc = './src/sass/*.scss';
+// Paths for src files and destinations
+var paths = {
+    styles: {
+      src: './src/sass/*.scss',
+      dest: './CSS/'
+    },
+    scripts: {
+      src: './src/js/*.js',
+      dest: './js/'
+    }
+  };
 
-// Minified and production ready .js & .css files
-const cssSrc = './CSS/';
-const jsSrc = './js/';
+
+//Clean build folders
+function clean() {
+    return del([paths.styles.dest + "*.css", paths.scripts.dest + "*.js"]).then(paths => {
+        console.log('Clearing css & javascript files');
+    });
+}
+exports.clean = clean;
 
 // Gulp sass
 function sass() {
-    return gulp.src(scssAppSrc)
+    return gulp.src(paths.styles.src)
     .pipe(gulpSass().on('error', gulpSass.logError))
+    .pipe(sourcemaps.init())
     .pipe(prefix({
         browsers: ['last 2 versions', '> 1%', 'ie 8', 'ie 7'],
         cascade: true,
         grid: "autoplace" 
     }))
-    .pipe(sourcemaps.init())
     .pipe(cleanCSS())
     .pipe(rename({
         suffix: '.min'
     }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(cssSrc));
+    .pipe(gulp.dest(paths.styles.dest));
 }
 
+// Javascript min
 function js_min() {
     return gulp
-    .src(jsAppSrc)
+    .src(paths.scripts.src)
+    .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(rename({
         suffix: '.min'
     }))
-    .pipe(gulp.dest(jsSrc));
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.scripts.dest));
 }
 
+// Watch scss and js files
 function watch() {
-    gulp.watch(scssAppSrc, { ignoreInitial: false }, series(sass, js_min));
+    gulp.watch(paths.styles.src, { ignoreInitial: false }, sass);
+    gulp.watch(paths.scripts.src, { ignoreInitial: false }, js_min);
 }
-
 exports.watch = watch;
+
+
+var build = gulp.series(clean, gulp.parallel(sass, js_min));
+exports.build = build;
+
+exports.default = build;
