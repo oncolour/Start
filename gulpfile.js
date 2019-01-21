c920c01f3f8fcfe20cd8b45bf5cbba922d740aec
oscarn <oscar.nilsson@sis.se>
2019-01-21 09:49:02 +0100

    adding autp-prefixer and js uglify

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ const gulpSass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require("gulp-rename");
+const prefix = require("gulp-autoprefixer");
+const uglify = require("gulp-uglify");
 //const { watch, series } = require('gulp');
  
 gulpSass.compiler = require('node-sass');
@@ -20,11 +22,13 @@ const jsSrc = './js/';
 
 // Gulp sass
 function sass() {
-
-return gulp.src(scssAppSrc)
-    .pipe(gulpSass({
-        
-    }).on('error', gulpSass.logError))
+    return gulp.src(scssAppSrc)
+    .pipe(gulpSass().on('error', gulpSass.logError))
+    .pipe(prefix({
+        browsers: ['last 2 versions', '> 1%', 'ie 8', 'ie 7'],
+        cascade: true,
+        grid: "autoplace" 
+    }))
     .pipe(sourcemaps.init())
     .pipe(cleanCSS())
     .pipe(rename({
@@ -32,11 +36,20 @@ return gulp.src(scssAppSrc)
     }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(cssSrc));
+}
 
+function js_min() {
+    return gulp
+    .src(jsAppSrc)
+    .pipe(uglify())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest(jsSrc));
 }
 
 function watch() {
-    gulp.watch(scssAppSrc, { ignoreInitial: false }, series(sass));
+    gulp.watch(scssAppSrc, { ignoreInitial: false }, series(sass, js_min));
 }
 
 exports.watch = watch;
---
+++ src/js/main-javascript.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var test = "this is a test variable";
+
+function myFunction() {
+    return test;
+}
