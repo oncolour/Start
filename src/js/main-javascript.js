028e2565e03998fe739bd53c974d5f455b7af503
oscarn <oscar.nilsson@sis.se>
2019-01-25 15:26:46 +0100

    adding smooth scroll script

--- src/js/main-javascript.js
+++ src/js/main-javascript.js
@@ -5,3 +5,8 @@ var test = "this is a test variable";
 function myFunction() {
     return test;
 }
+
+// Adding smooth and realistic scrolling behaviour. 
+$(function() {
+    jQuery.scrollSpeed(100, 800);
+});
